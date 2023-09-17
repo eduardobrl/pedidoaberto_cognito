212c815ce866572a6f1b9ccf69527ecd1fafb9d2
eduardobrl <eduardobrl10@gmail.com>
2023-09-16 22:02:52 -0300

    ajusta fmt

--- main.tf
+++ main.tf
@@ -112,8 +112,8 @@ resource "aws_cognito_user_pool_domain" "this" {
 }
 
 resource "aws_cognito_user_pool_ui_customization" "example" {
-  client_id = aws_cognito_user_pool_client.pedidoaberto_frontend_client.id
-  css        = ".label-customizable {font-weight: 400;}"
+  client_id    = aws_cognito_user_pool_client.pedidoaberto_frontend_client.id
+  css          = ".label-customizable {font-weight: 400;}"
   user_pool_id = aws_cognito_user_pool_domain.this.user_pool_id
 }
 
