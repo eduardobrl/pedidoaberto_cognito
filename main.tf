506b258de8f337f8f0e4ee60ff6def63a7a8a6a2
eduardobrl <eduardobrl10@gmail.com>
2023-09-16 22:18:25 -0300

    ajusta urls

--- main.tf
+++ main.tf
@@ -102,9 +102,9 @@ resource "aws_cognito_user_pool_client" "pedidoaberto_frontend_client" {
 
   user_pool_id = aws_cognito_user_pool.usuario_user_pool.id
 
-  default_redirect_uri = "localhost"
-  callback_urls        = ["localhost"]
-  logout_urls          = ["localhost"]
+  default_redirect_uri = "http://localhost"
+  callback_urls        = ["http://localhost"]
+  logout_urls          = ["http://localhost"]
 
   generate_secret     = false
   explicit_auth_flows = ["ALLOW_USER_SRP_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
